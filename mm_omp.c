168dc7f227b0c21759fd212ed0e4ba1473ca9a7e
Felipe Freitas <felipesafreitas@gmail.com>
2018-09-04 02:25:32 -0300

    CHANGED size as terminal parameter

--- mm_omp.c
+++ mm_omp.c
@@ -50,26 +50,24 @@ void printMatrix(int** matrix, int l, int c)
 // Implementação ikj
 int** multMatrix(int** A, int** B, int al, int ac, int bl, int bc)
 {
-    int r, i, k, j;
+    int r;
     int **C;
     C = generateMatrix(al, bc, 0);
     
     double st=omp_get_wtime();
 
-    printf("Numero maximo de threads: %i \n", omp_get_max_threads());
-
-    #pragma omp parallel for private(k,j)
-    for(i = 0; i < al; i++)
-    {
-        for(k = 0; k < ac; k++)
+        #pragma omp parallel
+        for(int i = 0; i < al; i++)
         {
-            r = A[i][k];
-            for(j = 0; j < bc; j++)
+            for(int k = 0; k < ac; k++)
             {
-                C[i][j] += r*B[k][j];
+                r = A[i][k];
+                for(int j = 0; j < bc; j++)
+                {
+                    C[i][j] += r*B[k][j];
+                }
             }
         }
-    }
     
     double en=omp_get_wtime();
     printf("Tempo Multiplicacao: %lf\n",en-st);
@@ -77,31 +75,37 @@ int** multMatrix(int** A, int** B, int al, int ac, int bl, int bc)
     return C;
 }
 
-int main (){
+int main (int argc, char *argv[]){
     int al = 0, ac = 0, bl = 0, bc = 0;
     int** A;
     int** B;
     int** C;
 
-    printf("Digite a dimensao da matriz A: (Ex. 3 2)\n");
-    scanf("%d %d", &al, &ac);
+    if(argc < 1)
+    {
+        printf("Digite a dimensao da matriz A: (Ex. 3 2)\n");
+        scanf("%d %d", &al, &ac);
 
-    printf("\nDigite a dimensao da matriz B: (Ex. 2 3)\n");
-    scanf("%d %d", &bl, &bc);
+        printf("\nDigite a dimensao da matriz B: (Ex. 2 3)\n");
+        scanf("%d %d", &bl, &bc);        
+    }
+    else 
+    {
+        al = ac = bl = bc = atoi(argv[1]);
+    }
 
     printf("\nDimensoes escolhidas: A[%d][%d] e B[%d][%d]\n", al, ac, bl, bc);
-    
-    printf("Gerando matrizes com valores randomicos...\n");
-    printf("Matrix A:\n");
+
     A = generateMatrix(al, ac, 1);
+    // printf("Matrix A:\n");
     //printMatrix(A, al, ac);
 
-    printf("Matrix B:\n");
     B = generateMatrix(bl, bc, 1);
+    // printf("Matrix B:\n");
     //printMatrix(B, bl, bc);
 
     printf("Calculando...\n\n");
-    printf("Matrix C:\n");
     C = multMatrix(A, B, al, ac, bl, bc);
+    // printf("Matrix C:\n");
     //printMatrix(C, al, bc);
 }
