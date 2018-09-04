bd3cfccd73f65213183afd1caf71f8ba71e8efc0
Felipe Freitas Santos Alves de Freitas <felipe.santos.alves@ibm.com>
2018-09-04 01:41:49 -0300

    ADDED mult for matrix simple

---
+++ mm.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int** generateMatrix(int l, int c, int populate)
+{
+    int** matrix;
+    matrix = (int**) calloc(l, 1+sizeof(int*));
+    
+    for(int i = 0;i<l;i++) 
+    {
+        matrix[i] = (int*) calloc(c, 1+sizeof(int));
+    }
+
+    matrix[l] = NULL;
+
+    for(int i = 0;i<l;i++) 
+    {
+        for(int j = 0;j<c;j++) 
+        {
+            if(populate == 1)
+            {
+                matrix[i][j] = rand() % 20;
+            }
+            else 
+            {
+                matrix[i][j] = NULL;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+void printMatrix(int** matrix, int l, int c)
+{
+    for(int i = 0; i < l; i++)
+    {
+        for(int k = 0; k < c; k++)
+        {
+            printf(" %d ", matrix[i][k]); 
+        }    
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+// Implementação ikj
+int** multMatrix(int** A, int** B, int al, int ac, int bl, int bc)
+{
+    int r;
+    int **C;
+    C = generateMatrix(al, bc, 0);
+    
+    for(int i = 0; i < al; i++)
+    {
+        for(int k = 0; k < ac; k++)
+        {
+            r = A[i][k];
+            for(int j = 0; j < bc; j++)
+            {
+                C[i][j] += r*B[k][j];
+            }
+        }
+    }
+
+    return C;
+}
+
+int main (){
+    int al = 0, ac = 0, bl = 0, bc = 0;
+    int** A;
+    int** B;
+    int** C;
+
+    printf("Digite a dimensao da matriz A: (Ex. 3 2)\n");
+    scanf("%d %d", &al, &ac);
+
+    printf("\nDigite a dimensao da matriz B: (Ex. 2 3)\n");
+    scanf("%d %d", &bl, &bc);
+
+    printf("\nDimensoes escolhidas: A[%d][%d] e B[%d][%d]\n", al, ac, bl, bc);
+    
+    printf("Gerando matrizes com valores randomicos...\n");
+    printf("Matrix A:\n");
+    A = generateMatrix(al, ac, 1);
+    printMatrix(A, al, ac);
+
+    printf("Matrix B:\n");
+    B = generateMatrix(bl, bc, 1);
+    printMatrix(B, bl, bc);
+
+    printf("Calculando...\n\n");
+    printf("Matrix C:\n");
+    C = multMatrix(A, B, al, ac, bl, bc);
+    printMatrix(C, al, bc);
+
+}
